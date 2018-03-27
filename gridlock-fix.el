a454e1f332abbf76e63fba7048e152db6a70f912
Dan Harms <danielrharms@gmail.com>
2018-03-27 17:41:43 -0500

    gridlock-fix getting closer to prime-time

--- gridlock-fix.el
+++ gridlock-fix.el
@@ -3,7 +3,7 @@
 ;; Author: Dan Harms <enniomore@icloud.com>
 ;; Created: Thursday, March 22, 2018
 ;; Version: 1.0
-;; Modified Time-stamp: <2018-03-26 08:41:52 dharms>
+;; Modified Time-stamp: <2018-03-27 17:41:27 dharms>
 ;; Modified by: Dan Harms
 ;; Keywords: tools
 ;; URL: https://github.com/articuluxe/gridlock.git
@@ -29,6 +29,12 @@
 ;;; Code:
 (require 'gridlock)
 
+(defvar gridlock-fix-init-hook '()
+  "Hooks run after setting up `gridlock-fix-mode' but before inspecting buffer.")
+
+(defvar-local gridlock-fix-metadata (make-hash-table :size 500)
+  "The metadata for this fix buffer.")
+
 (defun gridlock-fix--read-file-into-list-of-lines (file)
   "Read FILE into a list of strings split line by line."
   (interactive "f")
@@ -40,38 +46,44 @@
   "Insert into HASH fix metadata from FILE."
   (let ((lines (gridlock-fix--read-file-into-list-of-lines file))
         arr str)
-    (dolist (line lines hash)
+    (dolist (line (cdr lines) hash)
       (setq arr (split-string line ","))
       (setq str (format "%s (%s) %s %s"
-                        (nth arr 1)
-                        (nth arr 2)
-                        (nth arr 3)
-                        (nth arr 4)))
-      (puthash (string-to-number (nth arr 0))
+                        (nth 1 arr)
+                        (nth 2 arr)
+                        (nth 3 arr)
+                        (nth 4 arr)))
+      (puthash (string-to-number (nth 0 arr))
                str hash))))
+
+(defun gridlock-fix-load-metadata-from-file (file)
+  "Read fix metadata from a csv file FILE."
+  (interactive "fInput csv file: ")
+  (if (file-exists-p file)
+      (gridlock-fix-metadata-read-file gridlock-fix-metadata file)
+    (error "%s does not exist" file)))
 
 (defcustom gridlock-fix-preferred-display-schemes
   '("popup" "pos-tip" "quick-peek" "echo")
   "Preferred display schemes for `gridlock-fix-mode'.
 Display schemes will be loaded in this order.")
 
-(defvar gridlock-fix-init-hook '()
-  "Hooks run after setting up `gridlock-fix-mode' but before inspecting buffer.")
-
-(defvar-local gridlock-fix-metadata (make-hash-table :size 500)
-  "The metadata for this fix buffer.")
-
 (defun gridlock-fix--reset-metadata ()
   "Reset the metadata associated with the current buffer in `gridlock-fix-mode'."
-  (clrhash gridlock-fix-metadata))      ;todo needed?
+  ;(clrhash gridlock-fix-metadata)
+  )
 
 (defun gridlock-fix--get-buffer-metadata ()
   "Initialize the buffer's metadata for `gridlock-fix-mode'."
-  (gridlock-fix-metadata-read-file gridlock-fix-metadata "fix4.2-out.csv"))
+  ;; (gridlock-fix-metadata-read-file gridlock-fix-metadata
+  ;;                                  (concat load-file-name
+  ;;                                          "/fix4.2-out.csv"))
+  )
 
 ;; field class override for fix
 (defclass gridlock-fix-field (gridlock-field)
   ((tag :initarg :tag
+        :initform 0
         :type (integer 0 *)
         :documentation "The fix tag's numeric value."))
   "A fix field of interest.")
@@ -86,17 +98,21 @@ It stretches from BEG to END, has index INDEX, and value STR."
   (let ((field (gridlock-fix-field
                 :begin beg :end end
                 :index index))
+        (values (split-string str "="))
         tag value)
-    ;; todo
-    (oset field tag tag)
-    (oset field string value)
-  ))
+    (setq tag (string-to-number (car values)))
+    (when (> tag 0)
+      (oset field tag tag))
+    (setq value (cadr values))
+    (when value
+      (oset field string value))
+    field))
 
 (defun gridlock-fix-get-title (field)
   "Return the title associated with FIELD."
   (let* ((tag (gridlock-fix-get-tag field))
          (str (gethash tag gridlock-fix-metadata)))
-    "field"))
+    str))
 
 (defun gridlock-fix-gather-titles (fields)
   "Return a list of all field headings in field list FIELDS."
@@ -128,8 +144,8 @@ It stretches from BEG to END, has index INDEX, and value STR."
   (if gridlock-fix-mode
       (progn
         (setq gridlock-anchor-regex "FIX\\.[45]")
-        (setq gridlock-field-delimiter "|")
-        (setq gridlock-field-regex-begin "8=")
+        (setq gridlock-field-delimiter "\x01")
+        (setq gridlock-field-regex-begin "\\(Send\\|Recv\\)([^)]+)")
         (setq gridlock-field-regex-end "10=.+")
         (setq gridlock-metadata-get-title-func #'gridlock-fix-get-title)
         (setq gridlock-metadata-gather-titles-func #'gridlock-fix-gather-titles)
